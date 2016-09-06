7b310b6713dbf859a4533e840c995aea260bbf4f
bvdlingen <bvdl@Berts-MacBook-Pro.local>
2016-09-06 10:27:27 +0200

    init
    Get python script, set permissions, run twice, once to Netherlands /
    Amsterdam

---
+++ speedtest-instalandrun.sh
@@ -0,0 +1,12 @@
+
+wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
+chmod a+rx speedtest_cli.py
+mv speedtest_cli.py /usr/local/bin/speedtest-cli
+chown root:root /usr/local/bin/speedtest-cli
+speedtest-cli
+speedtest-cli --server 3386
+
+
+
+
+
