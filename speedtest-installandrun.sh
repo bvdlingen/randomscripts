e1b15ca1312fcc08243897519c7b40ec1652858e
bvdlingen <bvdl@me.com>
2018-04-14 10:27:37 +0200

    Update speedtest-installandrun.sh

--- speedtest-installandrun.sh
+++ speedtest-installandrun.sh
@@ -1,6 +1,7 @@
-# speed_cli.py has been depricated
-# wget https://raw.github.com/sivel/speedtest-cli/master/speedtest_cli.py
-# replaced by new version
+# notes: 
+# 1. speed_cli.py has been depricated, replaced by new version
+# 2. Please not that most Linux distributions have speedtest included in their package repository
+#    Solus Linux example: eopkg install speedtest-cli
 
 #get
 wget https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
