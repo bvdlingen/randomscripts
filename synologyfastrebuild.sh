ee690c74e1b0776cc3ff9427013b73bb16904d3b
Bert <bvdl@me.com>
2018-06-17 23:17:38 +0200

    Create synologyfastrebuild.sh

---
+++ synologyfastrebuild.sh
@@ -0,0 +1,6 @@
+echo 16384 >/sys/block/md2/md/stripe_cache_size
+echo 45000 > /proc/sys/dev/raid/speed_limit_max
+echo 45000 > /proc/sys/dev/raid/speed_limit_min
+
+#monitor progress
+while true; do clear; cat /proc/mdstat; sleep 2; done
