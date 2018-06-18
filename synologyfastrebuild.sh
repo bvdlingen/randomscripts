ef58c7d0e08cbeb41c4aa1ffaf9637cb5931becb
Bert <bvdl@me.com>
2018-06-18 06:57:41 +0200

    Update synologyfastrebuild.sh

--- synologyfastrebuild.sh
+++ synologyfastrebuild.sh
@@ -1,9 +1,10 @@
  # read https://forum.synology.com/enu/viewtopic.php?t=121532
  
  
-echo 16384 >/sys/block/md2/md/stripe_cache_size
+echo 32768 >/sys/block/md2/md/stripe_cache_size
 echo 45000 > /proc/sys/dev/raid/speed_limit_max
 echo 45000 > /proc/sys/dev/raid/speed_limit_min
+echo 32768 > /sys/block/md2/queue/read_ahead_kb
 
 #monitor progress
 while true; do clear; cat /proc/mdstat; sleep 2; done
