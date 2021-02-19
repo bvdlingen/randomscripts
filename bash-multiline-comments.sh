ed2668709b82b60f89f0ad84cedc1951c7ef69c2
Bert <bvdl@me.com>
2021-02-19 22:18:34 +0100

    Create bash-multiline-comments.sh

---
+++ bash-multiline-comments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo precomment
+
+<< ////
+Usage:
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+Looks like this is the most simple method to do multiline comments
+
+////
+
+echo postcomment
