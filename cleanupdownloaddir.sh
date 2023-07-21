791a6e6c9d0e2d55beb5b3b8bd07178aaa05962c
Bert <16510400+bvdlingen@users.noreply.github.com>
2023-07-21 13:10:31 +0200

    Update and rename cleanupdownloaddir to cleanupdownloaddir.sh
    Ta-da! Check out the slick upgrades:
    
        Comments galore! Now the script speaks for itself. No more decoding cryptic lines.
        Behold the variable magic! Neat and tidy for future generations to marvel at.
        Avoiding file collisions like a pro! No accidental mix-ups in the file migration dance.
        Informative chit-chats! You'll be serenaded with updates as we whip your downloads into shape.
    
    Just a friendly reminder: Before launching the show, tweak those source_dir and target_dir paths to fit your grand SSD adventure. Now, let the cleanup commence, and watch as your downloads are whisked away to the speedy SSD realm!

---
+++ cleanupdownloaddir.sh
@@ -0,0 +1,39 @@
+ 
+```bash
+#!/bin/bash
+
+echo precomment && : << ~_~
+Ta-da! Check out the slick upgrades:
+
+    Comments galore! Now the script speaks for itself. No more decoding cryptic lines.
+    Behold the variable magic! Neat and tidy for future generations to marvel at.
+    Avoiding file collisions like a pro! No accidental mix-ups in the file migration dance.
+    Informative chit-chats! You'll be serenaded with updates as we whip your downloads into shape.
+
+Just a friendly reminder: Before launching the show, tweak those source_dir and target_dir paths to fit your grand SSD adventure. Now, let the cleanup commence, and watch as your downloads are whisked away to the speedy SSD realm!
+~_~ && echo postcomment
+
+# Check if the target directory exists
+target_dir="/Volumes/SamsungSSD/Downloads"
+if [ -d "$target_dir" ]
+then
+    # If the target directory exists, move all files from the source directory
+    source_dir="/Users/bvdl/Downloads"
+    echo "Moving files from $source_dir to $target_dir ..."
+    
+    # Use 'mv' with the '-n' flag to avoid overwriting existing files with the same name
+    mv -n "$source_dir"/* "$target_dir"/
+
+    echo "Cleanup completed! Your downloads are now on the speedy SSD."
+else
+    echo "The target directory $target_dir does not exist. Make sure your SSD is connected."
+fi
+```
+
+Improvements made:
+1. Added comments to explain the purpose of each section in the script.
+2. Stored the target and source directories in variables to make the script more readable and maintainable.
+3. Added a check using the `-n` flag with `mv` to avoid overwriting existing files with the same name in the target directory.
+4. Provided better feedback messages to inform the user about the progress and completion of the cleanup process.
+
+Please make sure to review and adjust the `source_dir` and `target_dir` paths according to your actual setup before running the script.
