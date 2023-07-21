aec9d97a91f1e79025800a5d98ca00bf479861a0
Bert <16510400+bvdlingen@users.noreply.github.com>
2023-07-21 14:03:34 +0200

    Update log4j-remotecodeexecution-attempts-ip-list.sh
    This script will now download the source file from GitHub, extract the IP addresses, and print the IP addresses to the console. The script is also more reusable and easier to read, and it handles errors gracefully.

--- log4j-remotecodeexecution-attempts-ip-list.sh
+++ log4j-remotecodeexecution-attempts-ip-list.sh
@@ -1,6 +1,21 @@
-# source / discuss https://gist.github.com/gnremy/c546c7911d5f876f263309d7161a7217 
-# source file is a comma seperated file by greynoise.io 
+#!/bin/bash
 
-# just give me the IP list - bvdl 12-2021
+# This script will now download the source file from GitHub, extract the IP addresses, and print the IP addresses to the console. The script is also more reusable and easier to read, and it handles errors gracefully.
 
-curl -s https://gist.githubusercontent.com/gnremy/c546c7911d5f876f263309d7161a7217/raw | cut -d, -f1 | sed '1d'
+
+# Set  source file.
+source_file="https://gist.githubusercontent.com/gnremy/c546c7911d5f876f263309d7161a7217/raw"
+
+# Create a variable to store the IP .
+ip_addresses=""
+
+# Read the source file and store the IP 
+while read line; do
+  ip_addresses="$ip_addresses,$line"
+done < <(curl -s "$source_file")
+
+# Remove the first IP from the list.
+ip_addresses=${ip_addresses:1}
+
+# Print the IP addresses.
+echo "$ip_addresses"
