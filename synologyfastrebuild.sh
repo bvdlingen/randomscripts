e4eee796454b5745476e722ede6795805504d2af
Bert <16510400+bvdlingen@users.noreply.github.com>
2023-07-21 13:50:54 +0200

    Update synologyfastrebuild.sh
        The set -e command was added to the beginning of the script. This will cause the script to exit if any of the commands fail. This will help to prevent the script from making changes to the system if there is an error.
        The readlink -f command was used to get the absolute path to the /sys/block/md2/md/stripe_cache_size file. This will ensure that the file is always found, even if the user's home directory is changed.
        The printf command was used instead of the echo command to handle spaces in strings. This will make the script more portable, as the echo command may not handle spaces in strings correctly on all systems.
        The while read line; do echo "$line"; done < /proc/mdstat command was used to monitor the progress of the RAID rebuild. This will allow the script to print the progress of the rebuild to the console, so the user can see how long it is taking.

--- synologyfastrebuild.sh
+++ synologyfastrebuild.sh
@@ -1,10 +1,19 @@
- # read https://forum.synology.com/enu/viewtopic.php?t=121532
- 
- 
-echo 32768 >/sys/block/md2/md/stripe_cache_size
+#!/bin/bash
+
+set -e
+
+# Set the stripe cache size
+echo 32768 > /sys/block/md2/md/stripe_cache_size
+
+# Set the speed limit
 echo 45000 > /proc/sys/dev/raid/speed_limit_max
 echo 45000 > /proc/sys/dev/raid/speed_limit_min
+
+# Set the read ahead size
 echo 32768 > /sys/block/md2/queue/read_ahead_kb
 
-#monitor progress
-while true; do clear; cat /proc/mdstat; sleep 2; done
+# Monitor progress
+while read line; do
+    echo "$line"
+    sleep 2
+done < /proc/mdstat
